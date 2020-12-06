f22f97ac96a9ecb9b55f28530d5e83ffe48416da
Keir Forster <keirforster@gmail.com>
2020-12-05 20:29:39 -0800

    build(client): add navbar component

--- client/projects/default/src/app/app.component.spec.ts
+++ client/projects/default/src/app/app.component.spec.ts
@@ -1,13 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+
 import { AppComponent } from './app.component';
 import { CardComponent } from './demo/card/card.component';
+import { NavbarComponent } from './demo/navbar/navbar.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [RouterTestingModule],
-      declarations: [AppComponent, CardComponent],
+      declarations: [AppComponent, CardComponent, NavbarComponent],
     }).compileComponents();
   });
 
--- client/projects/default/src/app/app.module.ts
+++ client/projects/default/src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CardComponent } from './demo/card/card.component';
 import { HeroComponent } from './demo/hero/hero.component';
+import { NavbarComponent } from './demo/navbar/navbar.component';
 
 @NgModule({
-  declarations: [AppComponent, CardComponent, HeroComponent],
+  declarations: [AppComponent, CardComponent, HeroComponent, NavbarComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
